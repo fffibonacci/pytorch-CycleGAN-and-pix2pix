f92ad84e32c7a90028f98fe8e18dc84b5084c3ad
Zack <szm0219@gmail.com>
2019-01-01 15:08:14 -0600

    up

--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/cityscapes --name cityscapes_pix2pix_v2_self_ephoch400 --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --no_lsgan --norm batch --pool_size 0 --batch_size 10 --niter 100 --niter_decay 100 --display_id 0
+python train.py --dataroot ./datasets/cityscapes --name cityscapes_pix2pix_v2_self_ephoch400 --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --no_lsgan --norm batch --pool_size 0 --batch_size 10 --niter 200 --niter_decay 200 --display_id 0
